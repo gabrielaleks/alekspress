e3bd7e7cfb09c76328c805686946228da38349c3
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2024-11-22 02:05:52 +0100

    Improve headers validation and create specific getters

--- src/http/request/headers.cpp
+++ src/http/request/headers.cpp
@@ -1,33 +1,61 @@
 #include "headers.hpp"
+#include "utils/string_utils.h"
 #include <iostream>
 
 namespace http {
     namespace request {
+        const std::unordered_set<std::string> Headers::headers_to_combine = {
+            "accept",
+            "connection"
+        };
+
+        const std::unordered_set<std::string> Headers::mandatory_headers {
+            "host"
+        };
+        
         Headers::Headers(
             std::unordered_map<std::string, std::string> headers
         ) : _headers(std::move(headers)) {
+            validate();
         }
 
-        /**
-         * To do: 
-         *  - Handle lower and upper casing (headers are case-insensitive)
-         *  - Implement validation for required and common headers
-         *  - Handle repeated headers: some should be combined, some should remain separate and some should use the last value
-         */
-        Headers Headers::from_vector(std::vector<std::string> headers_vector) {
+        void Headers::validate() const {
+            for (const auto& mandatory_header : mandatory_headers) {
+                if (_headers.find(mandatory_header) == _headers.end()) {
+                    throw std::invalid_argument("Missing mandatory header: " + mandatory_header);
+                }
+            }
+        }
+
+        Headers Headers::from_vector(const std::vector<std::string>& headers_vector) {
+            if (headers_vector.empty()) {
+                throw std::invalid_argument("Headers vector cannot be empty");
+            }
+            
             Headers headers;
             std::unordered_map<std::string, std::string> headers_map;
             for (const auto& line : headers_vector) {
-                size_t keyEnd = line.find(": ", 0);
+                size_t keyEnd = line.find(": ");
 
                 if (keyEnd == std::string::npos) {
                     throw std::invalid_argument("Invalid HTTP header: " + line);
                 }
 
-                std::string key = line.substr(0, keyEnd);
+                std::string key =  line.substr(0, keyEnd);
+                utils::StringUtils::to_lowercase(key);
+
                 std::string value = line.substr(keyEnd + 2);
-                headers_map[key] = value;
+
+                // Check for key's existence in map
+                if (headers_map.find(key) != headers_map.end()) {
+                    // Case 1: comma-separated headers
+                    if (Headers::headers_to_combine.find(key) != Headers::headers_to_combine.end()) {
+                        value = headers_map[key] + ", " + value;
+                    }
+                }
+                headers_map[std::move(key)] = std::move(value);
             }
+
             return Headers(headers_map);
         }
 
@@ -35,6 +63,31 @@ namespace http {
             for (const auto& [key, value] : _headers) {
                 std::cout << key << ": " << value << std::endl;
             }
+        }
+
+        std::string Headers::get_header(const std::string &header) const {
+            auto it = _headers.find(header);
+            return it != _headers.end() ? it->second : "";
+        }
+
+        std::string Headers::host() const {
+            return get_header("host");
+        }
+
+        std::string Headers::content_length() const {
+            return get_header("content-length");
+        }
+
+        std::string Headers::content_type() const {
+            return get_header("content-type");
+        }
+
+        std::string Headers::user_agent() const {
+            return get_header("user-agent");
+        }
+
+        std::string Headers::accept() const {
+            return get_header("accept");
         }
     }
 }
--- src/http/request/headers.hpp
+++ src/http/request/headers.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <unordered_set>
 
 namespace http {
     namespace request {
@@ -13,11 +14,25 @@ namespace http {
                 Headers(std::unordered_map<std::string, std::string> headers);
 
                 std::unordered_map<std::string, std::string> all() const { return _headers; }
+
+                std::string get_header(const std::string &header) const;
+
+                std::string host() const;
+                std::string content_length() const;
+                std::string content_type() const;
+                std::string user_agent() const;
+                std::string accept() const;
+                
                 void print_all();
 
-                static Headers from_vector(std::vector<std::string> headers_vector);
+                static Headers from_vector(const std::vector<std::string>& headers_vector);
             private:
                 std::unordered_map<std::string, std::string> _headers;
+
+                static const std::unordered_set<std::string> headers_to_combine;
+                static const std::unordered_set<std::string> mandatory_headers;
+
+                void validate() const;
         };
     }
 }
