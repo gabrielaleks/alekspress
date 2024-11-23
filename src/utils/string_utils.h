955e8bfb11babd0cf7eac3157c1337337024f47d
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2024-11-23 14:25:25 +0100

    Add to_uppercase function

--- src/utils/string_utils.h
+++ src/utils/string_utils.h
@@ -17,5 +17,17 @@ namespace utils {
 
                 return result;
             }
+
+            static std::string to_uppercase(const std::string& str) {
+                std::string result = str;
+                std::transform(
+                    result.begin(),
+                    result.end(),
+                    result.begin(),
+                    [](unsigned char c) { return std::toupper(c); }
+                );
+
+                return result;
+            }
     };
 }
