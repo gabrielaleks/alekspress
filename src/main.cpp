3f6a05e6a51c19064313aca60bd2ca065b9ed369
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2024-11-24 22:53:12 +0100

    Add example usage

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,22 @@
 
 int main() {
     server::Alekspress app(8080);
+
+    /** Example usage of Alekspress
+     * 
+     * app.get("/hello", [](const server::Request& req) -> server::Response {
+     *   return server::Response::ok("Hello, World!");
+     * });
+     * 
+     * app.post("/user", [](const server::Request& req) -> server::Response {
+     *   ...
+     *   server::Response response;
+     *   response.set_body("created user!");
+     *   response.set_header("my-header", "was-added");
+     *   response.set_status_code(200);
+     *   return response;
+     * })
+     */
+
     app.run();
 }
