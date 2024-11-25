607a0c3ed4c3f9c3ee9689234c7cd91252f6e915
Gabriel Aleksandravicius <gabriel.aleks19@gmail.com>
2024-11-25 01:26:51 +0100

    Use routes

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,19 @@
 #include <http_server/alekspress.hpp>
 
 int main() {
-    server::Alekspress app(8080);
+    alekspress::Alekspress app(8080);
 
-    /** Example usage of Alekspress
-     * 
-     * app.get("/hello", [](const server::Request& req) -> server::Response {
-     *   return server::Response::ok("Hello, World!");
-     * });
-     * 
-     * app.post("/user", [](const server::Request& req) -> server::Response {
-     *   ...
-     *   server::Response response;
-     *   response.set_body("created user!");
-     *   response.set_header("my-header", "was-added");
-     *   response.set_status_code(200);
-     *   return response;
-     * })
-     */
+    app.get("/hello", [](const alekspress::Request&) -> alekspress::Response {
+        return alekspress::Response::ok("Hello world!");
+    });
+
+    app.post("/", [](const alekspress::Request& req) -> alekspress::Response {
+        alekspress::Response resp;
+
+        resp.set_body(req.body());
+
+        return resp;
+    });
 
     app.run();
 }
